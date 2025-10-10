32d23a6eabe433039085324d6c0af6b58d0e259b
SaYaNa1234567 <121336430+SaYaNa1234567@users.noreply.github.com>
2025-10-10 11:11:32 +0900

    Update l2.py

--- labs/lab3/l2.py
+++ labs/lab3/l2.py
@@ -1,7 +1,12 @@
-x = int(input("Введите число от 1 до 9:"))
-if x < 1 or x > 9:
-    print("Ошибка.")
-    x = int(input("Введите число от 1 до 9:"))
+while True:
+    try:
+        x = int(input("Введите число от 1 до 9:"))
+        if x >= 1 and x <= 9:
+            break
+        else:
+            print("Ошибка")
+    except error:
+        print("Число должно быть в диапазоне от 1 до 9!")
 for i in range(1,11):
     res = x * i
     print(f"{x} * {i} = {res}")
