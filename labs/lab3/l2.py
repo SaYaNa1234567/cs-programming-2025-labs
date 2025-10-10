8170fa54ff961fb24cc71b72f7a9ca3bdbee8c9e
SaYaNa1234567 <121336430+SaYaNa1234567@users.noreply.github.com>
2025-10-10 11:03:58 +0900

    Update l2.py

--- labs/lab3/l2.py
+++ labs/lab3/l2.py
@@ -1,4 +1,8 @@
 x = int(input("Введите число от 1 до 9:"))
+if x < 1 or x > 9:
+    print("Ошибка.")
+    x = int(input("Введите число от 1 до 9:"))
 for i in range(1,11):
     res = x * i
     print(f"{x} * {i} = {res}")
+
