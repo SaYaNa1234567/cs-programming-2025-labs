f14cee3453085a4e3693a07b914531db9e0c542d
SaYaNa1234567 <121336430+SaYaNa1234567@users.noreply.github.com>
2025-12-18 12:43:03 +0900

    Delete labs/lab6 directory

--- labs/lab6/l1.py
+++
@@ -1,9 +0,0 @@
-value = float(input("Количество:"))
-orig = input("Часы/минуты(h/m):")
-
-if orig == 'h':
-    print(value * 60, "m")
-elif orig == 'm':
-    print(value / 60, "h")
-else:
-    print(value)
--- labs/lab6/l5.py
+++
@@ -1,9 +0,0 @@
-text = input()
-cleaned = ""
-for c in text:
-    if c.isalpha():
-        cleaned += c.lower()
-if cleaned == cleaned[::-1]:
-    print("Да")
-else:
-    print("Нет")
