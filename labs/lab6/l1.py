0c27c6a3883979abb6d09f21821b54a3da393957
SaYaNa1234567 <121336430+SaYaNa1234567@users.noreply.github.com>
2026-01-16 11:26:22 +0900

    Add files via upload

--- labs/lab6/l1.py
+++ labs/lab6/l1.py
@@ -1,9 +1,18 @@
-value = float(input("Количество:"))
-orig = input("Часы/минуты(h/m):")
+v, f, t = input("").split()
+v = int(v)
+if f == 'h' and t == 'm':
+    print(v*60, 'm')
+elif f == 'h' and t == 's':
+    print(v*3600, 's')
+elif f == 'm' and t == 'h':
+    print(v//60, 'h')
+elif f == 'm' and t == 's':
+    print(v*60, 's')
+elif f == 's' and t == 'h':
+    print(v//3600, 'h')
+elif f == 's' and t == 'm':
+    print(v//60, 'm')
 
-if orig == 'h':
-    print(value * 60, "m")
-elif orig == 'm':
-    print(value / 60, "h")
-else:
-    print(value)
+    
+
+
